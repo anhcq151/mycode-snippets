15ab5167afa38fef35b4e654292bf98f53f92706
anhcq151 <quanganh151@gmail.com>
2020-11-04 21:26:07 +0700

    first commit

---
+++ PowerShell/Get-Folder.ps1
@@ -0,0 +1,49 @@
+function GetFolder {
+    param (
+        [Parameter()]
+        [ValidateScript({Test-Path $_})]
+        [string]
+        $FolderPath
+    )
+    
+    begin {
+        if (!$FolderPath) {
+            $FolderPath = Get-Location
+        }
+    }
+
+    process {
+        $Folder = Get-ChildItem -Path $FolderPath
+        if ($null -eq $Folder) {
+            continue
+        }
+        else {
+            $ReportObj = @()
+            foreach ($item in $Folder) {
+                $ItemObj = [PSCustomObject]@{}
+                try {
+                    $SizeInMB = [math]::Round((Get-ChildItem $item.PSPath -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB, 2)
+                    $ItemObj = [PSCustomObject]@{
+                        Name = $item.Name
+                        Mode = $item.Mode
+                        SizeInMB = $SizeInMB
+                    }
+                }
+                catch {
+                    $ItemObj = [PSCustomObject]@{
+                        Name = $item.Name
+                        Mode = $item.Mode
+                        SizeInMB = 0
+                    }
+                }
+                $ReportObj += $ItemObj
+            }
+        }
+    }
+
+    end {
+        $FolderPathSize = [math]::Round((Get-ChildItem $FolderPath -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB, 2)
+        Write-Host "$FolderPath size is: $FolderPathSize MB"
+        $ReportObj | Sort-Object -Property SizeInMB -Descending
+    }
+}
